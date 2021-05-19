832fd1c5b445ca84b4d27539d1094f51e1d38ea8
qwer2415wy <qwer2415wy@gmail.com>
2021-05-20 06:30:55 +0800

    CH2D

--- main.js
+++ main.js
@@ -124,7 +124,46 @@ function create(date,temperature){
 }
 
 //清空表格
+function clean(){
+    var table = document.getElementsByTagName('table')[0]
+    while(table.rows.length > 1){
+        table.deleteRow(1)
+    }
+}
+
+//把表格輸出成Excel
 function fnExcelReport()
 {
-    var rootRef = firebase.database().ref("https://ch2d-99336-default-rtdb.firebaseio.com");
+    var tab_text="<table border='3px'><tr bgcolor='#FFFFFF'>";
+    var textRange; var j=0;
+    tab = document.getElementById('headerTable'); // id of table
+
+    for(j = 0 ; j < tab.rows.length ; j++) 
+    {     
+        tab_text=tab_text+tab.rows[j].innerHTML+"</tr>";
+        //tab_text=tab_text+"</tr>";
+    }
+
+    tab_text=tab_text+"</table>";
+    tab_text= tab_text.replace("日期","Date")
+    tab_text= tab_text.replace("體溫","Temperature")
+    tab_text= tab_text.replace(/<A[^>]*>|<\/A>/g, "");//remove if u want links in your table
+    tab_text= tab_text.replace(/<img[^>]*>/gi,""); // remove if u want images in your table
+    tab_text= tab_text.replace(/<input[^>]*>|<\/input>/gi, ""); // reomves input params
+
+    var ua = window.navigator.userAgent;
+    var msie = ua.indexOf("MSIE "); 
+
+    if (msie > 0 || !!navigator.userAgent.match(/Trident.*rv\:11\./))      // If Internet Explorer
+    {
+        txtArea1.document.open("txt/html","replace");
+        txtArea1.document.write(tab_text);
+        txtArea1.document.close();
+        txtArea1.focus(); 
+        sa=txtArea1.document.execCommand("SaveAs",true,"Say Thanks to Sumit.xls");
+    }  
+    else                 //other browser not tested on IE 11
+        sa = window.open('data:application/vnd.ms-excel,' +　encodeURIComponent(tab_text));  
+
+    return (sa);
 }
