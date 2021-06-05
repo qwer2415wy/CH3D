5dac6e8664c09ea609c9d6104e24863baaa414f8
qwer2415wy <qwer2415wy@gmail.com>
2021-06-05 18:53:38 +0800

    CH2D

--- main.js
+++ main.js
@@ -24,48 +24,56 @@ var StartTime = '20210514'
 //上傳資料
 function save(){
     if(document.getElementById('div1').style.display == "block"){display()}
+    var dateVal = document.getElementById('date').value
     var userId = document.getElementById('userId').value
     var temperature = document.getElementById('temperature').value
+    var date = Nowtime
     if(userId.length == 1){userId = "0" + userId}
     if(userId == "" || temperature == ""){
         alert("請輸入座號及今日體溫!!!")
     }
     else{
-        if(userId >= 0 && userId <= 34 && temperature > 30 && temperature < 45 && userId != "24"){
-            database.ref('DetailedRecords/' + Nowtime + '/' + userId ).once("value").then(function(snapshot){
-                var val = snapshot.val();
-                if(val == null){
-                    if(userId >= 0 && userId <= 35 && temperature > 30 && temperature < 45){
-                        if(temperature > 37.5){
-                            database.ref('DetailedRecords/' + Nowtime + '/' + userId).set({
-                                Temperature : temperature ,
-                                Warning : 1 ,
-                                Date : Nowtime
-                                })
-                            database.ref('Records/' + userId).child(Nowtime).set(temperature)
+        if(time.getDay() != 0 && time.getDay() != 6){
+            if(userId >= 0 && userId <= 34 && temperature > 30 && temperature < 45 && userId != "24"){
+                if(dateVal != ''){date = dateVal}
+                database.ref('DetailedRecords/' + date + '/' + userId ).once("value").then(function(snapshot){
+                    var val = snapshot.val();
+                    if(val == null){
+                        if(userId >= 0 && userId <= 35 && temperature > 30 && temperature < 45){
+                            if(temperature > 37.5){
+                                database.ref('DetailedRecords/' + date + '/' + userId).set({
+                                    Temperature : temperature ,
+                                    Warning : 1 ,
+                                    Date : date
+                                    })
+                                database.ref('Records/' + userId).child(date).set(temperature)
+                            }
+                            else{
+                                database.ref('DetailedRecords/' + date + '/' + userId).set({
+                                    Temperature : temperature ,
+                                    Warning : 0 ,
+                                    Date : date
+                                    })
+                                database.ref('Records/' + userId).child(date).set(temperature)
+                            }
+                            alert('體溫上傳成功')
+                            document.getElementById('temperature').value = ''
                         }
-                        else{
-                            database.ref('DetailedRecords/' + Nowtime + '/' + userId).set({
-                                Temperature : temperature ,
-                                Warning : 0 ,
-                                Date : Nowtime
-                                })
-                            database.ref('Records/' + userId).child(Nowtime).set(temperature)
-                        }
-                        alert('體溫上傳成功')
+                    }
+                    else{
+                        alert('體溫已回報完畢，不需要重複上傳')
                         document.getElementById('temperature').value = ''
                     }
-                }
-                else{
-                    alert('今日體溫已回報完畢，不需要重複上傳')
-                    document.getElementById('temperature').value = ''
-                }
-            })
+                })
+            }
+            else{
+                alert('座號或體溫輸入有誤')
+                document.getElementById('userId').value = ''
+                document.getElementById('temperature').value = ''
+            }
         }
         else{
-            alert('座號或體溫輸入有誤')
-            document.getElementById('userId').value = ''
-            document.getElementById('temperature').value = ''
+            alert('假日不能上傳體溫')
         }
     }
 }
@@ -87,19 +95,36 @@ function display(){
 //獲取資料庫中的個人的每日體溫紀錄
 function get(){
     clean()
+    var dateVal = document.getElementById('date').value
     var userId = document.getElementById('userId').value
-    if(userId >= 0 && userId <= 34 && userId != '' && userId != "24"){
+    if(userId >= 0 && userId <= 35 && userId != ''){
         if(userId.length == 1){userId = "0" + userId}
-        var end = Nowtime - StartTime + 1
         if(document.getElementById('div1').style.display != "block"){display()}
-        for(i = 0; i < end ; i++){
-            var Refdate = (parseInt(StartTime,10)+i).toString()
-            database.ref('DetailedRecords/' + Refdate + '/' + userId).once("value").then(function(snapshot){
+        if(dateVal != ''){
+            database.ref('DetailedRecords/' + dateVal + '/' + userId).once("value").then(function(snapshot){
                 var val = snapshot.val();
                 if(val != null){
                     create(val.Date,val.Temperature)
                 }
             })
+        }
+        else{
+            for(i = 0; i < 5; i++){
+                var dateTime = new Date()
+                dateTime = dateTime.setDate(dateTime.getDate()-i)
+                dateTime = new Date(dateTime)
+                var tm = dateTime.getMonth()+1
+                var td = dateTime.getDate()
+                if(tm < 10){tm = "0" + (dateTime.getMonth()+1)}
+                if(td < 10){td = "0" + dateTime.getDate()}
+                var dateString = "" + dateTime.getFullYear() + tm + td
+                database.ref('DetailedRecords/' + dateString + '/' + userId).once("value").then(function(snapshot){
+                    var val = snapshot.val();
+                    if(val != null){
+                        create(val.Date,val.Temperature)
+                    }
+                })
+            }
         }
     }
     else{
@@ -129,41 +154,4 @@ function clean(){
     while(table.rows.length > 1){
         table.deleteRow(1)
     }
-}
-
-//把表格輸出成Excel
-function fnExcelReport()
-{
-    var tab_text="<table border='3px'><tr bgcolor='#FFFFFF'>";
-    var textRange; var j=0;
-    tab = document.getElementById('headerTable'); // id of table
-
-    for(j = 0 ; j < tab.rows.length ; j++) 
-    {     
-        tab_text=tab_text+tab.rows[j].innerHTML+"</tr>";
-        //tab_text=tab_text+"</tr>";
-    }
-
-    tab_text=tab_text+"</table>";
-    tab_text= tab_text.replace("日期","Date")
-    tab_text= tab_text.replace("體溫","Temperature")
-    tab_text= tab_text.replace(/<A[^>]*>|<\/A>/g, "");//remove if u want links in your table
-    tab_text= tab_text.replace(/<img[^>]*>/gi,""); // remove if u want images in your table
-    tab_text= tab_text.replace(/<input[^>]*>|<\/input>/gi, ""); // reomves input params
-
-    var ua = window.navigator.userAgent;
-    var msie = ua.indexOf("MSIE "); 
-
-    if (msie > 0 || !!navigator.userAgent.match(/Trident.*rv\:11\./))      // If Internet Explorer
-    {
-        txtArea1.document.open("txt/html","replace");
-        txtArea1.document.write(tab_text);
-        txtArea1.document.close();
-        txtArea1.focus(); 
-        sa=txtArea1.document.execCommand("SaveAs",true,"Say Thanks to Sumit.xls");
-    }  
-    else                 //other browser not tested on IE 11
-        sa = window.open('data:application/vnd.ms-excel,' +　encodeURIComponent(tab_text));  
-
-    return (sa);
 }
