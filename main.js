b638a8b51020b67aad6905797b5d8036af619a68
qwer2415wy <qwer2415wy@gmail.com>
2021-06-07 08:53:49 +0800

    Update main.js

--- main.js
+++ main.js
@@ -81,8 +81,11 @@ function save(){
 
 //檢查日期
 function checkDate(dateP){
-    if(dateP.length == 8 || Number == null){
-        if(Number(dateP.slice(0,4)) > 2020 && Number(dateP.slice(4,6)) <= 12 && Number(dateP.slice(4,6)) > 0 && Number(dateP.slice(6,8)) <= 31 && Number(dateP.slice(6,8)) > 0 || Number == null){
+    if(dateP.length == 0){
+        return true
+    }
+    else if(dateP.length == 8){
+        if(Number(dateP.slice(0,4)) > 2020 && Number(dateP.slice(4,6)) <= 12 && Number(dateP.slice(4,6)) > 0 && Number(dateP.slice(6,8)) <= 31 && Number(dateP.slice(6,8)) > 0){
             return true
         }
         else{
